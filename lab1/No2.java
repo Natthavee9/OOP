a9e92983d6b504927e95d934b79e3c06a189b5b3
Natthavee9 <y.natthavee@gmail.com>
2026-06-27 18:15:33 +0700

    Refactor financial calculation class and method naming for clarity

--- lab1/No2.java
+++ lab1/No2.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
-class Med{
-      double finacial(double value){
-            double interest = 1+0.00417;
-            double total =0.0;
-            for(int i =1;i<7;i++){
+public class No2 {
+      
+      public double finacial(double value) {
+            double interest = 1 + 0.00417;
+            double total = 0.0;
+            for (int i = 1; i < 7; i++) {
                   total = (value + total) * interest;
             }
             return total;
-      }  
-}
+      }
 
-public class No2 {
       public static void main(String[] args) {
-            Med m = new Med();
+            No2 m = new No2();
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter the monthly saving amount:");
             double value = sc.nextInt();
             double total_sum = m.finacial(value);
-            System.out.printf("After the sixth month, the account value is $ %.2f%n",total_sum);
+            System.out.printf("After the sixth month, the account value is $%.2f%n",total_sum);
             sc.close(); 
       }    
 }
