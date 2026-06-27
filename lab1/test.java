ec3c1b0a5120e7884e97e7ce802bb5dd2b0c95f1
Natthavee9 <y.natthavee@gmail.com>
2026-06-27 15:33:43 +0700

    Add initial implementation of test class with input dialog

---
+++ lab1/test.java
@@ -0,0 +1,9 @@
+package lab1;
+
+import javax.swing.JOptionPane;
+public class test {
+      public static void main(String[] args) { // ต้องมีเมธอด main เพื่อให้โปรแกรมเริ่มทำงาน
+            String string = JOptionPane.showInputDialog(null, "TEst", "Dialog Title",
+                        JOptionPane.QUESTION_MESSAGE);
+      }
+}
