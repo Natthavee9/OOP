d82b3c4fbb51e70bdce3d11db72aa89098089d20
Natthavee9 <y.natthavee@gmail.com>
2026-07-18 00:23:00 +0700

    Add Rectangle class with overlap method and create RegularPolygon class

--- lab2/No7.java
+++ lab2/No7.java
@@ -1,11 +1,38 @@
 import java.util.Scanner;
 
+class Rectangle {
+      private double h, w, x, y;
+
+      public Rectangle(double h, double w, double x, double y) {
+            this.h = h;
+            this.w = w;
+            this.x = x;
+            this.y = y;
+      }
+
+
+      public double overlap(Rectangle r){
+            double dx =  Math.abs(this.x - r.x);
+            double dy =  Math.abs(this.y - r.y);
+
+      }
+
+
+
+
+
+}
+
 public class No7 {
       public static void main(String[] args) {
             Scanner sc =new Scanner(System.in);
             System.out.print("Enter r1 center x- ,y-condinate,width and height: ");
-            double 
+            double x1 = sc.nextDouble();
+            double y1 = sc.nextDouble();
+            double h1 = sc.nextDouble();
+            double w1 = sc.nextDouble();
+
           
       }
-      
+
 }
---
+++ lab2/No8.java
@@ -0,0 +1,31 @@
+class RegularPolygon{
+      private int n =3;
+      private double side = 1;
+      private double x =0;
+      private double y = 0;
+
+      RegularPolygon(){
+
+      }
+
+      public RegularPolygon(int n, double side, double x, double y) {
+            this.n = n;
+            this.side = side;
+            this.x = x;
+            this.y = y;
+      }
+
+      
+
+      
+      
+
+
+
+}      
+public class No8 {
+      public static void main(String[] args) {
+            
+      }
+      
+}
