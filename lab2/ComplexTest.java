87591a919d731c248758e69db1c20a5d499b8412
Natthavee9 <y.natthavee@gmail.com>
2026-07-18 15:33:01 +0700

    no1 Complextest

--- lab2/ComplexTest.java
+++ lab2/ComplexTest.java
@@ -1,40 +1,71 @@
-class Complex{
-      private double r,i;
+class Complex {
+  private double r, i;
 
-    Complex(double r, double i){
-      this.r =r ;
-      this.i =i;
-    }
-    Complex(Complex c){
-      this(c.r,c.i);
-    }
+  Complex(double r, double i) {
+    this.r = r;
+    this.i = i;
+  }
 
-    public void add(Complex c){
-      r+=c.r;
-      i+=c.i;
-    }
-    
-    public void minus(Complex c){
-      r-=c.r;
-      i-=c.i;      
-    }
+  Complex(Complex c) {
+    this(c.r, c.i);
+  }
 
-    public void multi(Complex c){
-      r*=c.r;
-      i*=c.i;
-    }
+  public void add(Complex c) {
+    this.r += c.r;
+    this.i += c.i;
+  }
 
-    public void print(){
-          System.out.println(r  + " + " + i + "i");
-    }
-      
+  public void minus(Complex c) {
+    this.r -= c.r;
+    this.i -= c.i;
+  }
+
+  public void multiply(Complex c) {
+    // (ac - bd) + (ad + bc)i
+    double tempR = (this.r * c.r) - (this.i * c.i);
+    double tempI = (this.r * c.i) + (this.i * c.r);
+    this.r = tempR;
+    this.i = tempI;
+  }
+
+  public void divide(Complex c) {
+    double denominator = Math.pow(c.r, 2) + Math.pow(c.i, 2);
+    double tempR = ((this.r * c.r) - (this.i * c.i)) / denominator;
+    double tempI = ((this.r * c.i) + (this.i * c.r)) / denominator;
+    this.r = tempR;
+    this.i = tempI;
+  }
+
+  public void print() {
+    System.out.println(r + " + " + i + "i");
+  }
+
 }
+
 class ComplexTest {
-      public static void main(String args[]) {
-            Complex a = new Complex(1.0, 2.0);
-            Complex b = new Complex(3.0, 4.0);
-            Complex c = new Complex(a);
-            c.add(b);
-            c.print();
-      }
+  public static void main(String args[]) {
+    Complex a = new Complex(1.0, 2.0);
+    Complex b = new Complex(3.0, 4.0);
+
+    System.out.print("Add: ");
+    Complex c1 = new Complex(a);
+    c1.add(b);
+    c1.print();
+
+    System.out.print("\nSubtraction: ");
+    Complex c2 = new Complex(a);
+    c2.minus(b);
+    c2.print();
+
+    System.out.print("\nMultiply: ");
+    Complex c3 = new Complex(a);
+    c3.multiply(b);
+    c3.print();
+
+    System.out.print("\nDivided: ");
+    Complex c4 = new Complex(a);
+    c4.divide(b);
+    c4.print();
+
+  }
 }
