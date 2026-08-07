73685f10fbf3f7addc568691c836d0ae76efa2e7
Natthavee9 <y.natthavee@gmail.com>
2026-08-08 01:58:03 +0700

    Refactor AscendSortFreq and minTwoset classes; update sorting and frequency calculation methods, and enhance input handling

--- lab4/no4_Ascend.java
+++ lab4/no4_Ascend.java
@@ -1,30 +1,78 @@
 class AscendSortFreq{
-      double[] A = new double[3];
+      private int[] A ;
 
-      AscendSortFreq(double[] a) {
-            A = a;
+      AscendSortFreq(int[] A) {
+            this.A = new int[A.length];
+            for(int i=0;i<A.length;i++){
+                  this.A[i] = A[i];
+            }
       }
 
-      public double AscendSort(double[] A){
-            int indx = 0;
-            int n = A.length;
-            int[] B = new int[n];
-            for(int i =0 ; i<n;i++){
-
+      public int[] AscendSort(int[] A){
+            int[]B  = new int[A.length];
+            for(int i=0;i<A.length;i++){
+                  B[i] = A[i];
             }
 
+            for (int i = 0; i < B.length - 1; i++) {
+
+                  for (int j = 0; j < B.length - 1 - i; j++) {
+
+                        if (B[j] > B[j + 1]) {
+
+                              int temp = B[j];
+                              B[j] = B[j + 1];
+                              B[j + 1] = temp;
+
+                        }
+
+                  }
+
+            }
+            return B;
       }
 
-      public double SortCommuFreq(double B){
+      public int[] SortCommuFreq(int[] B){
+            int[] C;
+            int count_not_again = 1;
+            for(int i=0;i<B.length-1;i++){
+                  if(B[i]!=B[i+1]){
+                        count_not_again++;
+                  }
+            } 
 
-      }
+            int n = count_not_again;
+            C = new int[n];
+            int idx = 0;
+            int count_n= 0;
 
-
-      
+            for(int i=0;i<B.length;i++){
+                  count_n++;
+                  if(i==B.length-1||B[i]!=B[i+1]){
+                        C[idx] = count_n;
+                        idx++;
+                  }
+            }
+            return C;
+      } 
 }
 public class no4_Ascend {
       public static void main(String[] args) {
-          
-      }
       
+            int[] A = {9,5,9,5,8};
+
+            AscendSortFreq a = new AscendSortFreq(A);
+            int[] B = a.AscendSort(A);
+            int[] C = a.SortCommuFreq(B);
+
+            int idx = 0;
+            for (int i = 0; i < B.length; i++) {
+                  // ตรวจสอบว่าเป็นตัวสุดท้าย หรือค่าไม่ซ้ำกับตัวถัดไป
+                  if (i == B.length - 1 || B[i] != B[i + 1]) {
+                        System.out.print(B[i]+"\n"+ C[idx]);
+      
+                        idx++;
+                  }
+            } 
+      }   
 }
--- lab4/no7_minTwoset.java
+++ lab4/no7_minTwoset.java
@@ -1,16 +1,40 @@
 import java.util.*;
 class minTwoset{
+      Scanner sc = new Scanner(System.in);
+      private  int[] arr;
       public int mintwoset(int[] arr ){
+            int total = 0;
+            int n = arr.length;
+            int result_min = 0;
+            for(int i = 0 ;i<n;i++){
+                  total+=arr[i];
+            }
+
+            for(int j=0;j<n;j++){
+                  for(int k = total;k>=0;k++){
+                        if()
+                  }
+            }
+
 
             
 
-            
+
+
+            return result_min;
+      }
+
+      public void input_arr(int[] a,int n){
+            for(int i =0 ;i<n;i++ ){
+                  a[i] = sc.nextInt();
+            }
       }
 }
 
 public class no7_minTwoset {
       public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
+          int n = sc.nextInt();
       }
       
 }
