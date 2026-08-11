de3bbf79bed45e8fe9cc8e0d0c5ccf578a9a33fa
Natthavee9 <y.natthavee@gmail.com>
2026-08-11 23:08:25 +0700

    Add Star class and main method in no2; initialize ArrayList and set up input handling

---
+++ lab5/no2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Star{
+      private ArrayList<String> str = new ArrayList<String>();
+      for(int i=0;i<str.size();i++){
+            
+      }
+}
+
+public class no2 {
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+      }
+      
+}
