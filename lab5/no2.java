238b052d82d0fa2f20a646c4e33a0a67e21b5283
Natthavee9 <y.natthavee@gmail.com>
2026-08-12 23:28:03 +0700

    Refactor Star class; streamline addStars and removeStars methods for better clarity and functionality

--- lab5/no2.java
+++ lab5/no2.java
@@ -1,7 +1,14 @@
 import java.util.*;
 class Star{
-      private ArrayList<String> str = new ArrayList<String>();
-      for(int i=0;i<str.size();i++){
+      public void addStars(ArrayList<String> str){
+            for(int i = 0 ;i<str.size();i=i+2){
+                  str.add(i+1,"*");
+            }
+      }
+
+      public void removeStars(ArrayList<String> str){
+            
+            str.removeIf(s -> s.equals("*"));    
             
       }
 }
@@ -9,7 +16,29 @@ class Star{
 public class no2 {
       public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-      }
-      
+            ArrayList<String> list = new ArrayList<String>();
+            while (sc.hasNext()) {
+                  if (sc.hasNextInt()) {
+                        break;
+                  }
+                  list.add(sc.next());
+            }
+
+            
+            Star s = new Star();
+
+            while(sc.hasNext()){
+                  int cmd =  sc.nextInt();
+
+                  if(cmd == 1){
+                        s.addStars(list);
+                  }
+                  else if(cmd == 2){
+                        s.removeStars(list);
+                  }
+
+                  System.out.println(list);
+            }
+            sc.close();
+      }  
 }
