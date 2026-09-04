3a59f41e4a996d8fca8d8a464f9e1d061e46bad5
Natthavee9 <y.natthavee@gmail.com>
2026-09-05 01:30:37 +0700

    Add lab7_no6 class; implement position handling and maze setup

---
+++ lab7/lab7_no6.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Positon{
+    int row ;
+    int col;
+    public Positon(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
+class MazeSol{
+    int m;
+}
+
+public class lab7_no6 {
+      public static void main(String[] args) {
+          Scanner sc =  new Scanner(System.in);
+
+          int M = sc.nextInt();
+          int N = sc.nextInt();
+
+          sc.close();
+      }
+}
