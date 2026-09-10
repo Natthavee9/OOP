b682b10071feac237173c82b968349596b144e44
Natthavee9 <y.natthavee@gmail.com>
2026-09-10 12:16:15 +0700

    Refactor USdollar conversion GUI; enhance layout and action listener for currency conversion

--- lab9/USdollar.java
+++ lab9/USdollar.java
@@ -1,4 +1,4 @@
-
+import java.awt.*;
 import javax.swing.*;
 
 class ConvertUS extends JFrame{
@@ -6,12 +6,43 @@ class ConvertUS extends JFrame{
       private JTextField jtUS = new JTextField();
       private JTextField jtCA = new JTextField();
 
+      public ConvertUS(){
+            setUI();
+            setDefault(this);
+            
+      }
 
-      
+      void setUI(){
+            JPanel jp = new JPanel(new GridLayout(2, 2));
+            jp.add(new JLabel("US Dollars"));
+            jp.add(jtUS);
+            jp.add(new JLabel("Canadian Dollars"));
+            jp.add(jtCA);
+            jtCA.setEditable(false);
 
+            JPanel pSouth = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+            pSouth.add(btConvert);
+            add(jp, BorderLayout.CENTER);
+            add(pSouth, BorderLayout.SOUTH);
+
+            btConvert.addActionListener((e) -> {
+                  double usd = Double.parseDouble(jtUS.getText());
+                  double cad = usd*1.5;
+                  jtCA.setText(String.format("%.2f", cad));
+            });
+
+      }
+
+      void setDefault(JFrame windows){
+            windows.setTitle("Convert US Dollars to Canadian Dollars");
+            windows.setSize(500, 150);
+            windows.setLocationRelativeTo(null);
+            windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            windows.setVisible(true);
+      }
 }
 public class USdollar {
       public static void main(String[] args) {
-          
+            ConvertUS conUS = new ConvertUS(); 
       }
 }
