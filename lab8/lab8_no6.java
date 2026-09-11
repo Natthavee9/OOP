37e845802210412d37d6a42c7af03edac3401a43
Natthavee9 <y.natthavee@gmail.com>
2026-09-11 16:23:15 +0700

    Fix constructor parameter name in MovablePoint; enhance MovableCircle implementation and add movement tests in main

--- lab8/lab8_no6.java
+++ lab8/lab8_no6.java
@@ -8,16 +8,16 @@ interface Movable{
 class MovablePoint implements Movable{
       int x,y;
       int xSpeed , ySpeed;
-	public MovablePoint(int x, int y, int xSpeed, int ySeed) {
+	public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
 		this.x = x;
 		this.y = y;
 		this.xSpeed = xSpeed;
-		this.ySeed = ySeed;
+		this.ySpeed = ySpeed;
 	}
 
       @Override 
       public String toString(){
-            return
+            return "(x,y) = " + "("+ x + ","+y+")" +" "+ "speed = (" + xSpeed+","+ySpeed+")";
       }
 
       @Override
@@ -39,28 +39,63 @@ class MovablePoint implements Movable{
       public void moveRight(){
             x+=xSpeed;
       }
-      
-
-
-      
 
 }
 
 class MovableCircle implements Movable{
       private int radius;
       private MovablePoint center;
-      public MovableCircle(int radius) {
-            super();
-            this.radius = radius;
-            
+      public MovableCircle(int x, int y, int xSpeed, int ySpeed,int radius) {
+            this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+            this.radius = radius; 
+      }
+
+      @Override 
+      public void moveUp(){
+            center.moveUp();
+      }
+
+      @Override
+      public void moveDown(){
+      }
+
+      @Override 
+      public void moveLeft(){
+            center.moveLeft();
+      }
+
+      @Override
+      public void moveRight(){
+            center.moveRight();
       }
       
-      
+      @Override
+      public String toString() {
+            return "radius = " + radius+" "+"center = " + center.toString();
 
+      }
 }
 public class lab8_no6 {
 
       public static void main(String[] args) {
+            System.out.println("MovablePoint");
+            MovablePoint m1 = new MovablePoint(0, 0, 2, 3);
+            System.out.println("start: " +m1.toString());
+
+            m1.moveUp();
+            System.out.println("moveUP(): " + m1.toString());
+            m1.moveLeft();
+            System.out.println("moveLeft(): " + m1.toString());
+
+            System.out.println();
+            System.out.println("MovableCircle");
+            MovableCircle c1 = new MovableCircle(10, 10, 5, 5, 20);
             
+            System.out.println("start circle: " + c1.toString());
+
+            c1.moveDown();
+            System.out.println("moveDown(): " + c1.toString());
+            c1.moveRight();
+            System.out.println("moveRight(): " + c1.toString());
       }
 }
